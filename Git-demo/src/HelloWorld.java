355e898112a5cf3a379b9e5f68e41f795e921790
RajaKumar2 <sitlab1112@gmail.com>
2017-04-22 23:54:06 -0400

    Added family members only

--- Git-demo/src/HelloWorld.java
+++ Git-demo/src/HelloWorld.java
@@ -2,9 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		// Dev 2- changed the display login
+		String[] names = {"Raj","Ashu","Anshu", "Avani"};
+		for(String name:names) {
+			System.out.println("Welcome: " + name);
+		}
+		
 	}
 
 }
